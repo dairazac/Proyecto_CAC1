8050e13843ee5e29de7c591f36cf8d438c90712d
dairazac <dairazacarias@gmail.com>
2022-11-01 16:09:22 -0300

    validaciones en js

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const valorTicket = 200;
+
+let descuentoEstudiante = 80;
+let descuentoTrainee = 50;
+let descuentoJunior = 15;
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let cantidad = document.getElementById("cantidad");
+let selectorcategoria = document.getElementById("selector");
+
+function totalAPagar(){
+
+    if (nombre.value === "") {
+        nombre.classList.add("is-invalid");
+        alert("Escriba su nombre");
+        return;
+    }
+    
+    else if (apellido.value === "") {
+        apellido.classList.add("is-invalid");
+        alert("Escriba su apellido");
+        return;
+    }
+
+    else if (email.value === "") {
+        email.classList.add("is-invalid");
+        alert("Escriba su email");
+        return;
+    }
+
+    else if (email.value === "") {
+        email.classList.add("is-invalid");
+        alert("Escriba su apellido");
+        return;
+    }    
+
+    else if ( (cantidad.value == 0) || (isNaN(cantidad.value)) ) {
+        cantidad.classList.add("is-invalid");
+        alert("Escriba una cantidad válida");
+        return;
+    }
+
+    else if (selectorcategoria.value == 0) {
+        selectorcategoria.classList.add("is-invalid");
+        alert("Elija una categoría");
+        return;
+    }
+
+    let precioTotal = (valorTicket * parseFloat(cantidad.value));
+
+    if (selectorcategoria.value == 1) {
+        precioTotal = precioTotal ;
+    }
+
+    else if (selectorcategoria.value == 2) {
+        precioTotal = ((descuentoEstudiante / 100) * precioTotal);
+    }
+
+    else if (selectorcategoria.value == 3) {
+        precioTotal = ((descuentoTrainee / 100) * precioTotal);
+    }
+
+    else if (selectorcategoria.value == 4) {
+        precioTotal = ((descuentoJunior / 100) * precioTotal);
+    }
+
+    console.log(precioTotal);
+}
+
+
+
+btnResumen.addEventListener('click', totalAPagar)
+
